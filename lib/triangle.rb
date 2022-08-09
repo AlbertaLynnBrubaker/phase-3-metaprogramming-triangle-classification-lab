f7849f4a1c540e74c45f5afb89ad5568abd1f911
Alberta Lynn Brubaker <findingalberta@gmail.com>
2022-08-09 11:23:24 -0500

    commit tree 37d4b1451304b4d3e47309d1347a1c29cb563c28

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,34 @@
 class Triangle
-  # write code here
+
+  def initialize (a, b, c)
+    @a = a
+    @b = b
+    @c = c
+    
+  end
+
+  def kind 
+    if (@a == @b && @a == @c)
+      :equilateral
+    elsif (@a == @b && @a != @c || @a == @c && @a != @b || @b == @c && @b != @a)
+      :isoceles
+    else
+      :scalene
+    end
+
+    # if @a <= 0 || @b <= 0 || @c <= 0
+    #   begin
+    #     raise TriangleError
+    #   end
+    # elsif @a + @b <= @c || @a + @c <= @b || @b + @c <= @a
+    #   begin
+    #     raise TriangleError
+    #   end
+    # end
+  end
+
+  class TriangleError < StandardError
+
+  end
+  
 end
